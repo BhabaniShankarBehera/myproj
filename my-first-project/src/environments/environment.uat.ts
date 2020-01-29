4fc362d40f60d5b054229f40630ffeca43e3ae8c
bhabani <bhabani.beher@happiestminds.com>
2020-01-29 14:09:37 +0530

    angular 8 spring boot integration

---
+++ my-first-project/src/environments/environment.uat.ts
@@ -0,0 +1,9 @@
+export const url= 'http://domain2.com';
+export const environment = {
+  production: false;
+  addProductPath: url+'/api/v1/products',
+  getProductPath: url+'/api/v1/getproducts',
+  getProductByIdPath: url+'/api/v1/getproduct?id=',
+  updateProductPath: url+'/api/v1/updateproduct?id=',
+  deleteProductPath: url+'/api/v1/deleteproduct?id='
+};
